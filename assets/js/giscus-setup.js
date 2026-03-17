6a15408fa4cfdc725caaaf9a4d7f8ae3eb3a3776
hongxiaoy <112234465+hongxiaoy@users.noreply.github.com>
2026-03-17 11:50:14 +0000

    Deploying to gh-pages from @ hongxiaoy/hongxiaoy.github.io@352a3e41e35d5763fc377484305bf5fe4fc71ab9 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "hongxiaoy/hongxiaoy-new.github.io",
+    "data-repo": "hongxiaoy/hongxiaoy.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
